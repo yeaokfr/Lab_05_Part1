86d5396d144e520cc581080b20060b1753a504bb
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-10-01 20:11:41 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double tax = 0.02;
+        System.out.println("How much does your item cost?");
+        int itemPrice = scan.nextInt();
+        double total = (double)itemPrice * tax;
+        if (itemPrice <= 100) {
+            System.out.println("Your total cost is: $" + ((double)itemPrice + total));
+        } else {
+            System.out.println("Your total cost is: $" + itemPrice);
+        }
+
     }
 }
